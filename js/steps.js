134b1fdb4b8161b03bd7bb23b0a7c6f7d5b619e4
Rolf van de Krol <info@rolfvandekrol.nl>
2015-09-09 08:35:06 +0200

    Reformat steps

--- js/steps.js
+++ js/steps.js
@@ -1,27 +1,23 @@
 
-var intro_example_go = {
-  attach: function(step) {
-    step.addClass('intro-example-go');
-  },
-  detachPrev: function(step) {
-    step.removeClass('intro-example-go');
-  }
-};
+var steps = [].concat(
+  [{ key: 'cover' }],
 
-var steps = [
-  { key: 'cover' },
-  { key: 'intro-1' },
-  jQuery.extend({ key: 'intro-1' }, intro_example_go),
-  { key: 'intro-2' },
-  jQuery.extend({ key: 'intro-2' }, intro_example_go),
-  { key: 'intro-3' },
-  jQuery.extend({ key: 'intro-3' }, intro_example_go),
-  { key: 'intro-4' },
-  jQuery.extend({ key: 'intro-4' }, intro_example_go),
-  { key: 'intro-5' },
-  jQuery.extend({ key: 'intro-5' }, intro_example_go),
-  { key: 'intro-6' },
-  jQuery.extend({ key: 'intro-6' }, intro_example_go),
-  { key: 'intro-7' },
-  jQuery.extend({ key: 'intro-7' }, intro_example_go),
-];
+  (function() {
+    var intro_example_go = {
+      attach: function(step) {
+        step.addClass('intro-example-go');
+      },
+      detachPrev: function(step) {
+        step.removeClass('intro-example-go');
+      }
+    };
+
+    var result = [];
+    for (var i = 1; i <= 7; i++) {
+      result.push({ key: 'intro-' + i});
+      result.push(jQuery.extend({ key: 'intro-' + i}, intro_example_go));
+    }
+
+    return result;
+  })()
+);
